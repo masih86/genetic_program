dea2c0979cc1b29fdda334a27895cbfc39dfc8b1
masih <masih.masih1386@gmail.com>
2023-10-02 06:13:02 +0330

    Update z.py

--- z.py
+++ z.py
@@ -3,16 +3,18 @@
 # importing packages
 import numpy as np
 import random
-
+# these are the factors
 age = np.random.randint(1, 10)
 humanity = np.random.randint(1, 10)
 skin_color = np.random.randint(1, 10)
 IQ = np.random.randint(1, 10)
 bliefe = np.random.randint(1, 10)
+#as far as we dont have more than one generation for now its our only generation
 first_generation = []
+#the first test of DNA changing is gonna be done on them
 new_generation = []
-tested_ones = []
-after_surgery = []
+
+# we prpduce our generation with this function randomly
 def best_person():
     best_age = np.random.choice(age)
 
@@ -22,30 +24,32 @@ def best_person():
     best_blf = np.random.choice(bliefe)
     best = [best_age, best_hum, best_skc, best_IQ, best_blf]
     first_generation.append(best)
-    # first_generation.remove("None")
 
-
+''' as far as we use function 
+the first generation list would be full of "None" valiues
+so this part of the code is about to delete them from list
+'''
+#its a number we se in algorithem
 num = 1
 
-second_generation = []
 for i in range(100):
     first_generation.append(best_person())
 for q in range(100):
     first_generation.pop(num)
     num = num + 1
 
+# random peopleto do the experiment
+
 for m in range(10):
     num = random.choice(first_generation)
     new_generation.append(num)
 
-
-a = 0
+# defineing their indexes as numbers
 index0 = new_generation[0]
 index1 = new_generation[1]
 index2 = new_generation[2]
 index3 = new_generation[3]
 index4 = new_generation[4]
-
 new_generation.clear()
 index0[0] = 10
 index1[0] = 10
@@ -55,13 +59,9 @@ index4[0] = 10
 
 new_generation = [index0,index1,index2,index3,index4]
 print(first_generation)
-a = 0
 
 def check_person():
-    a = 0
-    b = 0
-    c = 0
-    d = 0
+    
     v = 0
     for e in range(len(first_generation)):
      ex = first_generation[v]
